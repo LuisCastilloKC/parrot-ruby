415e01ad356142ceb33894fd59b6d80e756bbede
Luis Castillo <raul@luiss-mbp.myfiosgateway.com>
2020-12-09 21:08:04 -0500

    commit tree 47269b47676f294b5839088f8f7421b68f37a343

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot(name="Squawk!") # in quote the default phrase without any argument
+    puts "#{name}" # Given Phrase to the Parrot
+    return "#{name}" # return the given phrase to the console
+end
